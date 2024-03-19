9b7a07be9b7efe054d02fdcc379943a422fd7217
Heitor Dutra <heitorassumpcao@hotmail.com>
2024-03-19 19:54:10 -0300

    More exercises

--- exercicios.py
+++ exercicios.py
@@ -24,3 +24,15 @@ valor_1 = True
 valor_2 = True
 
 print(valor_2 and valor_1)
+
+# %%
+try:
+    data = input("Insira uma data no formato dd/mm/aaaa: ")
+    data_formatada = data.split("/")
+    for i in range(len(data_formatada)):
+        data_formatada[i]=int(data_formatada[i])
+    # data_formatada é uma lista
+    print(f"Dia: {data_formatada[0]}\nMês: {data_formatada[1]}\nAno:{data_formatada[2]}")
+except:
+    print("Não foi inserido um número")
+# %%
