b83f1791d23811ebdc448002fa8790f5b47534aa
Heitor Dutra <heitorassumpcao@hotmail.com>
2024-03-19 19:28:25 -0300

    Add exercises

---
+++ exercicios.py
@@ -0,0 +1,26 @@
+#%%
+# Divisão
+print(6/4)
+# %%
+# Valor inteiro da divisão
+print(6//4)
+# %%
+# Resto da divisão
+print(6%4)
+# %%
+nome_aluno = "Heitor"
+print(type(nome_aluno))
+
+# %%
+print(nome_aluno.lower())
+# %%
+print(nome_aluno.upper())
+# %%
+# Separando a string em um determinado caractere
+email = "nome@email.com"
+print(email.split("@"))
+# %%
+valor_1 = True
+valor_2 = True
+
+print(valor_2 and valor_1)
